bba0f5651be65d365a301d681220d3e351e737a2
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-30 13:33:55 -0400

    Finished the problem :)

--- match.js
+++ match.js
@@ -1,6 +1,6 @@
 var input = "A string with 7  numbers in it... 42 and 88, 123 and 16, 4 and 22.";
 var number = /\b(\d{2})\b/g;
 var match;
-// Character at is one off for some reason
+// Getting correct answer however my "fix" feels like cheap 
 while (match = number.exec(input))
-  print("Found", match[1], "at", match.index);
+  print("Found", match[1], "at", match.index - 1);
